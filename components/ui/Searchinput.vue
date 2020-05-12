a1db2ddd18c3ec9f624b6a1acdd838af73e445ad
Tatiana Kozlova <tanushakozlova@gmail.com>
2020-05-13 00:42:54 +0300

    Сверстала заголовок, форму для поиска по сайту, кнопки для пагинации, прописала стили для контейнера где будут карточки, стили пока что для 1440

---
+++ components/ui/Searchinput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input class="search-input" type="search" placeholder="Поиск" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.search-input {
+  width: 1074px;
+  height: 52px;
+  border: 1px solid #e8e8e8;
+  box-sizing: border-box;
+  margin-right: 20px;
+}
+</style>
--- pages/stories.vue
+++ pages/stories.vue
@@ -1,11 +1,98 @@
 <template>
-  <div>
-    Место кода Татьяны
-  </div>
+  <main class="stories">
+    <h2 class="stories-title">
+      Истории неизлечимых привычек
+    </h2>
+    <form class="search">
+      <searchinput class="search-input" />
+      <searchbutton>Поиск</searchbutton>
+    </form>
+    <section id="cards" class="cards">
+      <!-- делаем функцию карточек -->
+      место для карточек
+      <!-- делаем пагинацию - разбивку по страничкам -->
+      <ul class="pagination">
+        <li>
+          <vue-ads-pagination class="pagination-button">1</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">2</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">3</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">4</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">5</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">6</vue-ads-pagination>
+        </li>
+        <li>
+          <vue-ads-pagination class="pagination-button">7</vue-ads-pagination>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script>
-export default {};
+//импортнуть хедер
+import Button from '@/components/ui/Button';
+import Searchinput from '@/components/ui/Searchinput';
+import VueAdsPageButton from '@/components/ui/VueAdsPageButton';
+//сюда импортировать карточки, не забыть прописать в компоненты и на страничку
+//импортнуть футер
+
+export default {
+  components: {
+    searchbutton: Button,
+    searchinput: Searchinput,
+    'vue-ads-pagination': VueAdsPageButton,
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.stories {
+  padding-left: 60px;
+  padding-right: 60px;
+}
+.stories-title {
+  font-style: normal;
+  font-weight: 600;
+  font-size: 32px;
+  line-height: 36px;
+  max-width: 413px;
+  margin-top: 100px;
+}
+.search {
+  display: flex;
+  margin-top: 60px;
+  padding: 0;
+}
+.pagination {
+  list-style: none;
+  display: flex;
+  max-width: 466px;
+  margin: 140px auto 0;
+}
+
+@media screen and (max-width: 1280px) {
+  .stories {
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+  .stories-title {
+    font-size: 28px;
+    line-height: 32px;
+    max-width: 367px;
+    margin-top: 90px;
+  }
+  .search {
+    margin-top: 50px;
+  }
+}
+</style>
