57eebb3229485236d4bd75df0a5363a6cb2c913a
Tatiana Kozlova <tanushakozlova@gmail.com>
2020-05-20 10:17:14 +0300

    сделала функцию карточек, пагинацию, вынесла цикл карточек в отельный блок в своем пока не использовала, сделала сториес в сторе

--- store/stories.js
+++ store/stories.js
@@ -1,1 +1,162 @@
-const STORIES = {};
+export const state = () => ({
+  cards: [
+    {
+      id: 1,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '1Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+      fullstory:
+        '111Я из военной семьи. Отец хоть и не был военным сам, но нас всех держал в ежовых рукавицах. Думаю, поэтому мы и выросли такими ответственными. «Сейчас если кто-то из футболистов моей команды опаздывает — начинаю злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда: «Значит, точно проиграем». Такая болезненная пунктуальность уже не лечится».',
+    },
+    {
+      id: 2,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '2Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+      fullstory:
+        '222Я из военной семьи. Отец хоть и не был военным сам, но нас всех держал в ежовых рукавицах. Думаю, поэтому мы и выросли такими ответственными. «Сейчас если кто-то из футболистов моей команды опаздывает — начинаю злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда: «Значит, точно проиграем». Такая болезненная пунктуальность уже не лечится».',
+    },
+    {
+      id: 3,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '3Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 4,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '4Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+      fullstory:
+        'Я из военной семьи. Отец хоть и не был военным сам, но нас всех держал в ежовых рукавицах. Думаю, поэтому мы и выросли такими ответственными. «Сейчас если кто-то из футболистов моей команды опаздывает — начинаю злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда: «Значит, точно проиграем». Такая болезненная пунктуальность уже не лечится».',
+    },
+    {
+      id: 5,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '5Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 6,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '6Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 7,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '7Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 8,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '8Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 9,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '9Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 10,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '10Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 11,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '11Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 12,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '12Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 13,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '13Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 14,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '14Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 15,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '15Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 16,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '16Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 17,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '17Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+    {
+      id: 18,
+      image:
+        'https://images.unsplash.com/photo-1589447755874-2aa53c7f6dd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      title: '18Владимир Тен',
+      subtitle:
+        'Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.',
+    },
+  ],
+});
+
+export const mutations = {};
+
+export const getters = {
+  getCards(state) {
+    return state.cards;
+  },
+};
